78f91af4f33c93a948bd3352d3257eab32cbbc20
GorkemPalali <grkmsrk.grsk@gmail.com>
2024-03-05 17:24:42 +0300

    camp day10

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,5 @@
 ﻿using Core.DataAccess;
+using Entities.DTOs;
 using Entity.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ namespace DataAccess.Abstract
 {
     public interface IProductDal:IEntityRepository<Product>
     {
+        List<ProductDetailDto> GetProductDetails();
 
     }
 }
